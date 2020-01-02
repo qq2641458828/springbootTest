cece372edb061a8a98a6eef0cb39ddcd0f462635
qq2641458828 <2641458828@qq.com>
2020-01-02 10:23:46 +0800

    修改

--- src/main/java/com/bdqn/SpringbootTestApplication.java
+++ src/main/java/com/bdqn/SpringbootTestApplication.java
@@ -9,6 +9,7 @@ public class SpringbootTestApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootTestApplication.class, args);
 		System.out.println("第一次修改");
+		System.out.println("第三次修改");
 	}
 
 }
